8181b1b41d328f72e7ced9463c019d8b0345eb52
Dan Watkins <dwatkins@valpineware.com>
2016-02-21 17:09:22 -0600

    Added Configure.py script to let developers setup the repository after cloning. The script currently just allows the Weather Underground API Key to be set.

---
+++ Configure.py
@@ -0,0 +1,13 @@
+apiKey = input('Enter a valid Weather Underground API Key: ')
+
+apiKeyFilePath = "./Weather/Resources/WeatherUndergroundAPIKey.txt"
+
+with open(apiKeyFilePath, "w") as apiKeyFile:
+    print(apiKey, file=apiKeyFile)
+
+print('Set Weather Underground API Key to', apiKey, 'in file', apiKeyFilePath)
+print('Remember to never commit this file to source control. It is ignored by the .gitignore normally.')
+
+print('\n')
+
+print('Configure complete')
